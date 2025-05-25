377b51bbba768128c82be7061f2634c731d70f4e
nishanthnishi-0 <nishanthy.24.beai@acharya.ac.in>
2025-05-25 23:48:24 +0530

    Create Zig zag.py

---
+++ Zig zag.py
@@ -0,0 +1,10 @@
+#zigzag
+print("Nishanth M Y,USN:1AY24AI078,SEC:O")
+lines = 9
+max_spaces = lines 
+for i in range(lines):
+    if i <= max_spaces:
+        spaces = i
+    else:
+        spaces = lines - i - 1
+    print(" " * spaces + "*" * 8)
