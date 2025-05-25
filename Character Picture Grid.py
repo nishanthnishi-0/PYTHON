3d7a8ba086df5860ea25123b4affb3de01dda577
nishanthnishi-0 <nishanthy.24.beai@acharya.ac.in>
2025-05-25 23:41:20 +0530

    Create Character Picture Grid.py

---
+++ Character Picture Grid.py
@@ -0,0 +1,16 @@
+#charactergrid
+print("Nishanth M Y,USN:1AY24AI078,SEC:O")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
